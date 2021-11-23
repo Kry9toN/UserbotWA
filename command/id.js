d6636a51dc075716c1956e9af5183e172c760877
Dhimas Bagus Prayoga <dhimasbagusprayoga@gmail.com>
2021-11-24 00:57:20 +0700

    id: import command id

---
+++ command/id.js
@@ -0,0 +1,13 @@
+module.exports = {
+    name: 'id',
+    description: 'Menampilkan id user/group',
+    execute (wa, chat, pesan) {
+        const uid = wa.sender
+        if (wa.isGroup) {
+            const gid = client.groupId
+            wa.sendMessage(wa.from, `Id mu adalah: *${uid}*\nId group: *${gid}*`, MessageType.text)
+        } else {
+            wa.sendMessage(wa.from, `Id mu adalah: *${uid}*`, MessageType.text)
+        }
+    }
+}
