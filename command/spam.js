a0b6f1fd91445cfc717b04c1f4d867bc1527c733
Dhimas Bagus Prayoga <dhimasbagusprayoga@gmail.com>
2021-11-24 00:49:04 +0700

    status: optimize text input

--- command/spam.js
+++ command/spam.js
@@ -4,11 +4,12 @@ const { sleep } = require('../util/time.js')
 module.exports = {
     name: 'spam',
     aliases: ['sp'],
-    description: 'Spam pesan ke target !spam <jumlah> <pesan>',
+    description: 'Spam pesan ke target !spam <jumlah>|<pesan>',
     execute (wa, chat, pesan, args) {
-        const num = parseInt(args[0])
+        const arg = client.body.slice(5)
+        const num = parseInt(arg.split('|')[0].trim())
         for(let i = 0; i < num; i++) {
-            wa.sendMessage(wa.from, args[1], MessageType.text)
+            wa.sendMessage(wa.from, arg.split('|')[1], MessageType.text)
             .catch(console.error)
             sleep(1000)
         }
