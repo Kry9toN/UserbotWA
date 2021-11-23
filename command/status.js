d783de9719ee0ca1c72f622607c63a5b9bf3614c
Dhimas Bagus Prayoga <dhimasbagusprayoga@gmail.com>
2021-11-24 00:14:36 +0700

    status: fix agrument functions

--- command/status.js
+++ command/status.js
@@ -3,7 +3,7 @@ const { MessageType } = require('@adiwajshing/baileys')
 module.exports = {
     name: 'status',
     description: 'Mengbah status/bio profil',
-    execute (wa, args) {
+    execute (wa, chat, pesan, args) {
         const string = args.slice().join(' ')
         wa.setStatus(string)
         .then(wa.sendMessage(wa.from, text, MessageType.text))
